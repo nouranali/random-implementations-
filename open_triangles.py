d52d533ed7b60a0ab9edf5e3ce605814c95c141d
Nouran Ali <nouranalimohammed@gmail.com>
2020-07-27 16:55:13 +0200

    Create open_triangles.py

---
+++ open_triangles.py
@@ -0,0 +1,22 @@
+import networkx as nx
+from itertools import combinations
+
+# Define node_in_open_triangle()
+def node_in_open_triangle(G, n):
+    """
+    Checks whether pairs of neighbors of node `n` in graph `G` are in an 'open triangle' relationship with node `n`.
+    """
+    in_open_triangle = False
+
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+
+        # Check if n1 and n2 do NOT have an edge between them
+        if not G.has_edge(n1, n2):
+
+            in_open_triangle = True
+
+            break
+
+    return in_open_triangle
+
